6184fff408d11837852516ebd51782c197d7d219
Pavel Urbanovich <yrbik@mail.ru>
2021-12-19 13:22:03 +0300

    feat: finish task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let arr = n.toString().split('');
+    let re = /,/gi;
+    
+    if (arr[0] === '-' ) {
+        return n.toString().slice(1).split('').reverse().join().replace(re, '');
+    } if (+arr[arr.length - 1] === 0) {
+        return n.toString().slice(0, -1).split('').reverse().join().replace(re, '');
+    } else {
+        return arr.reverse().join().replace(re, '');
+    } 
 }
